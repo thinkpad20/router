ce4ab3928213f8fa0a8bfae1f8357e4100de1d5b
thinkpad20 <ithinkican@gmail.com>
2013-06-07 17:52:46 -0500

    don't know why double free error occurs, removing free

--- router/sr_icmp.c
+++ router/sr_icmp.c
@@ -326,7 +326,7 @@ void send_icmp_port_unreachable(struct sr_instance *sr,
     /* send */
     sr_send_packet(sr, new_packet, len, incoming_iface->name);  
     printf("Sent icmp timeout, new_packet is %p\n", new_packet); fflush(stdout);
-    free(new_packet);
+    /*free(new_packet);*/
     printf("freed our allocated packet\n"); fflush(stdout);
 }
 
